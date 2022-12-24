efe951be85e9865f5a5095bc9449c8c9cf329118
Maryna Karamanova <karamanova91@gmail.com>
2022-12-24 20:20:00 +0200

    tasks completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divRef = document.querySelector('.gallery');
 
+function createGaleryMarkup(items) {
+  return items
+    .map(
+      item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`,
+    )
+    .join('');
+}
+
+const addGalerryMarkup = createGaleryMarkup(galleryItems);
+
+divRef.innerHTML = addGalerryMarkup;
+divRef.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width ="800" height ="600">`,
+  );
+  instance.show();
+
+  divRef.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(item => {
+      return `<li><a class="gallery__item"
+     href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a></li>`;
+    })
+    .join('');
+}
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
 console.log(galleryItems);
